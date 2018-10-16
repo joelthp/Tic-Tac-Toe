44966e2937e20b35e2e6a79c8734ce1bd63d06fd
joelthp <alisjoel@gmail.com>
2018-10-16 21:43:05 +0200

    ajout morpion

--- morpion.rb
+++ morpion.rb
@@ -16,32 +16,32 @@ class Board < Boardcase
 # on definit le constructeur avec les variables d'instances
         puts "Joueurs créés...".colorize(:blue)
         puts "La partie va commencer...".colorize(:blue)
-        @win = false
-        @draw = false
-        @@turn = 1
-        display
+        @gagne = false
+        @egalite = false
+        @@tour = 1
+        show
     end
 
 # Explication du jeu
-    def display
-        puts "=== MORPION ===".colorize(:red)
+    def show
+        puts "****** MORPION ****** ".colorize(:red)
         puts "Il suffit d'entrer son numero de case".colorize(:red) 
         puts "Par exemple : A2".colorize(:red)           
-        puts "===================".colorize(:red)  
-        drawBoard
+        puts "****** ****** ****** ****** ".colorize(:red)  
+        afftableau
     end
 
 # On affiche le tableau
-    def drawBoard
+    def afftableau
         puts "===================".colorize(:blue)
-        puts "Tour #{@@turn}".colorize(:red)  
+        puts "Tour #{@@tour}".colorize(:red)  
         puts "     A    B    C    ".colorize(:blue)
         puts "   --------------  ".colorize(:red)  
         puts " |  #{@@row1[:a1]}  |  #{@@row1[:b1]}   |  #{@@row1[:c1]} | 1 ".colorize(:blue)
         puts "   --------------  ".colorize(:red)  
         puts " |  #{@@row2[:a2]}  |  #{@@row2[:b2]}   |  #{@@row2[:c2]} | 2 ".colorize(:blue)
         puts "   --------------  ".colorize(:red)  
-        puts " |  #{@@row3[:a3]}  |  #{@@row3[:b3]}   |  #{@@row3[:c3]} |  3 ".colorize(:blue)
+        puts " |  #{@@row3[:a3]}  |  #{@@row3[:b3]}   |  #{@@row3[:c3]} | 3 ".colorize(:blue)
 
     end
 
@@ -50,8 +50,8 @@ class Board < Boardcase
         @@rows.each do |row|
             row.each do|key, value|
                 if key == casse && value == ' '
-                   if @@turn.odd? then row[key] = "X"  else row[key] = "O"  end
-                    @@turn += 1
+                   if @@tour.odd? then row[key] = "X"  else row[key] = "O"  end
+                    @@tour += 1
                 end
             end
         end
@@ -59,8 +59,8 @@ class Board < Boardcase
 
 # On decrit les combinaisons gagnantes.
     def check_board
-        x_wins = ["X", "X", "X"]
-        o_wins = ["O", "O", "O"]
+        x_gagnew = ["X", "X", "X"]
+        o_gagnew = ["O", "O", "O"]
 
         winning_rows = [            
             [@@row1[:a1],@@row2[:a2],@@row3[:a3]],
@@ -74,33 +74,33 @@ class Board < Boardcase
         ]
 
         winning_rows.each do |match|
-            if match == x_wins || match == o_wins
-                @win = true
+            if match == x_gagnew || match == o_gagnew
+                @gagne = true
             end
         end
 
-        if @@turn >=9
-            @draw = true
+        if @@tour >=9
+            @egalite = true
         end
     end
 
     def win
-        @win
+        @gagne
     end 
 
 
     def draw
-        @draw
+        @egalite
     end        
 
 end
 
 
 class Player
-    attr_accessor :name
+    attr_accessor :nom
 
-    def initialize(name)
-        @name = name
+    def initialize(nom)
+        @nom = nom
     end
 end
 
@@ -108,8 +108,8 @@ end
 class Game
 # on cree 2 joueurs et un tableau.
     def initialize
-        @player1 = Player.new(player_name(1))
-        @player2 = Player.new(player_name(2))
+        @player1 = Player.new(player_nom(1))
+        @player2 = Player.new(player_nom(2))
         @current_player = @player2
 
         @selected_values = []
@@ -120,7 +120,7 @@ class Game
     end
  
  # on demande aux joueurs leurs nom avant de commencer.
-    def player_name(num)
+    def player_nom(num)
         puts "Joueur numero #{num}, quel est votre nom ?"
         gets.chomp
     end
@@ -129,7 +129,7 @@ class Game
     def play_game
         while @game_on
         # On demande au joueur de choisir une case 
-            puts "Quel case choisis tu #{@current_player.name} ?".colorize(:yellow)  
+            puts "Quel case choisis tu #{@current_player.nom} ?".colorize(:yellow)  
             cell = gets.chomp.to_sym.downcase
         # Si une case est deja prise, on demande au joueur d'en choisir une autre
            while cell.match(/[^a-c1-3]/) || @selected_values.include?(cell)
@@ -139,10 +139,10 @@ class Game
 
             @selected_values << cell
             @board.select(cell)
-            @board.drawBoard
+            @board.afftableau
             @board.check_board
             if @board.win
-                puts "Le gagnant est #{@current_player.name}!".colorize(:yellow)  
+                puts "Le gagnant est #{@current_player.nom}!".colorize(:yellow)  
                exit
             elsif @board.draw
                 puts "Egalite !".colorize(:yellow)  
